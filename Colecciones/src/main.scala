f758d272c4cacb93d6ed2d938694a0c5388610da
Apollo D. Jackson <43951787+jorgecabrejas7@users.noreply.github.com>
2021-05-16 14:01:53 +0200

    ParColl Done

--- Colecciones/src/main.scala
+++ Colecciones/src/main.scala
@@ -1,10 +1,10 @@
-
+import scala.collection.parallel.immutable._
 
 object main {
   
   
   
-  def imprimir(a:Int,lista:List[Int]) {//a es un contador para saber en que posicion nos encontramos
+  def imprimir(a:Int,lista:ParSeq[Int]) {//a es un contador para saber en que posicion nos encontramos
 
 	if(lista==Nil){
 	  
@@ -27,12 +27,12 @@ object main {
    //...
 
    
-     def leerColumna(l: List[Int], i: Int, a: Int, long: Int, l2: List[Int]): List[Int] = {
+     def leerColumna(l: ParSeq[Int], i: Int, a: Int, long: Int, l2: ParSeq[Int]): ParSeq[Int] = {
 	  if(a < long)
 	  		if(l.tail == Nil)
 	  		{
 	  		  if((i%long) == a){
-	  		    val lAux = l2 ::: List(l.head)
+	  		    val lAux = l2 ++ ParSeq(l.head)
 	  			  lAux
 	  		  }else{
 	  		    val lAux = l2 
@@ -42,26 +42,26 @@ object main {
 	  		}
 	  		else
 	  			if((i%long) == a)
-	  				leerColumna(l.tail, (i+1), a, long, l2 ++ List(l.head))
+	  				leerColumna(l.tail, (i+1), a, long, l2 ++ ParSeq(l.head))
 	  			else
 	  				leerColumna(l.tail, (i+1), a, long, l2)
 	  	else
-	  		List(-1)
+	  		ParSeq(-1)
   }
      
      
-  def leerFila(l: List[Int], i: Int, a: Int, f: Int, l2: List[Int]): List[Int] = {
+  def leerFila(l: ParSeq[Int], i: Int, a: Int, f: Int, l2: ParSeq[Int]): ParSeq[Int] = {
   	if(a < f)
   		if(l== Nil){
   		  val lAux = l2;
   			lAux;
   		}else
   			if((i/f) == a)
-  				leerFila(l.tail, (i+1), a, f, l2 ++ List(l.head))
+  				leerFila(l.tail, (i+1), a, f, l2 ++ ParSeq(l.head))
   			else
   				leerFila(l.tail, (i+1), a, f, l2)
   	else
-  		List(-1)
+  		ParSeq(-1)
   }
   
 
@@ -73,8 +73,7 @@ def todas(x: Int, y : Int): Int = {
      }
   
 
-  
-  def seguidos(long : Int,n:Int , fila:List[Int]): Int = {
+ def seguidos(long : Int,n:Int , fila:ParSeq[Int]): Int = {
 	val l = fila.length;  //Longitud de la lista
 	if(n==long){            //Si es el final de la iteracion para
 		0;
@@ -96,36 +95,36 @@ def todas(x: Int, y : Int): Int = {
   
   
   
-  def matrizFilas(l1: List[Int], l2: List[Int], l3: List[Int], l4: List[Int], l5: List[Int], l6: List[Int], l7: List[Int]): List[Int] = {
-	val matriz = List(l1,l2,l3,l4,l5,l6,l7).par.reduce(_++_)
+  def matrizFilas(l1: ParSeq[Int], l2: ParSeq[Int], l3: ParSeq[Int], l4: ParSeq[Int], l5: ParSeq[Int], l6: ParSeq[Int], l7: ParSeq[Int]): ParSeq[Int] = {
+	val matriz = ParSeq(l1,l2,l3,l4,l5,l6,l7).reduce(_++_)
 	matriz
 }
   
   
-  def matrizColumnas(c1: List[Int], c2: List[Int], c3: List[Int], c4: List[Int], c5: List[Int], c6: List[Int], c7: List[Int], c8: List[Int], c9: List[Int], matriz: List[Int]): List[Int] = {
+  def matrizColumnas(c1: ParSeq[Int], c2: ParSeq[Int], c3: ParSeq[Int], c4: ParSeq[Int], c5: ParSeq[Int], c6: ParSeq[Int], c7: ParSeq[Int], c8: ParSeq[Int], c9: ParSeq[Int], matriz: ParSeq[Int]): ParSeq[Int] = {
 	if(c1.tail == Nil)
 	{
-		val matrizAux = List(matriz,List(c1.head),List(c2.head),List(c3.head),List(c4.head),List(c5.head),List(c6.head),List(c7.head),List(c8.head),List(c9.head)).par.reduce(_++_)
+		val matrizAux = ParSeq(matriz,ParSeq(c1.head),ParSeq(c2.head),ParSeq(c3.head),ParSeq(c4.head),ParSeq(c5.head),ParSeq(c6.head),ParSeq(c7.head),ParSeq(c8.head),ParSeq(c9.head)).reduce(_++_)
 		matrizAux
 	}
 	else
 	{
-    val matrizAux = List(matriz,List(c1.head),List(c2.head),List(c3.head),List(c4.head),List(c5.head),List(c6.head),List(c7.head),List(c8.head),List(c9.head)).par.reduce(_++_)
+    val matrizAux = ParSeq(matriz,ParSeq(c1.head),ParSeq(c2.head),ParSeq(c3.head),ParSeq(c4.head),ParSeq(c5.head),ParSeq(c6.head),ParSeq(c7.head),ParSeq(c8.head),ParSeq(c9.head))reduce(_++_)
     matrizColumnas(c1.tail, c2.tail, c3.tail, c4.tail, c5.tail, c6.tail, c7.tail, c8.tail, c9.tail, matrizAux)
   }
 }   
   
-  def comparaMatrices(l1: List[Int], l2: List[Int], r: List[Int]): List[Int] = {
+  def comparaMatrices(l1: ParSeq[Int], l2: ParSeq[Int], r: ParSeq[Int]): ParSeq[Int] = {
 	if(l1.tail == Nil)
 	{
 		if(l1.head == l2.head)
 		{
-			val rfinal = r ++ List(l1.head)
+			val rfinal = r ++ ParSeq(l1.head)
 			rfinal
 		}
 		else
 		{
-			val rfinal = r ++ List(0)
+			val rfinal = r ++ ParSeq(0)
 			rfinal
 		}
 	}
@@ -133,102 +132,102 @@ def todas(x: Int, y : Int): Int = {
 	{
 		if(l1.head == l2.head)
 		{
-			comparaMatrices(l1.tail, l2.tail, r ++ List(l1.head))
+			comparaMatrices(l1.tail, l2.tail, r ++ ParSeq(l1.head))
 		}
 		else
 		{
-			comparaMatrices(l1.tail, l2.tail, r ++ List(0))
+			comparaMatrices(l1.tail, l2.tail, r ++ ParSeq(0))
 		}
 	}
 } 
   
  
   
-  def generarTablero(n:Int,lista:List[Int]): List[Int] = {
+  def generarTablero(n:Int,lista:ParSeq[Int]): ParSeq[Int] = {
 		if(n==0){
 			lista
 		}else{
 			val r = scala.util.Random;
 			val color = 1 + r.nextInt(6);
-			val aux:List[Int] = List(color);
-			lista ::: generarTablero(n-1,lista) ::: aux ;
+			val aux:ParSeq[Int] = ParSeq(color);
+			lista ++ generarTablero(n-1,lista) ++ aux ;
 		}
 	}                                         //> generarTablero: (n: Int, lista: List[Int])List[Int]
 	
 	
-	def mover(n :Int ,posicion: Int , movimiento: Int,tablero :List[Int]): List[Int] = {
+	def mover(n :Int ,posicion: Int , movimiento: Int,tablero :ParSeq[Int]): ParSeq[Int] = {
   	if(posicion>7*9){
-  		List(); //Error la posicion no existe
+  		ParSeq(); //Error la posicion no existe
   	}else if (posicion < 0){
-  		List(); //Error la posicion no existe
+  		ParSeq(); //Error la posicion no existe
   	}else if(movimiento>7*9){
-  		List(); //Error la posicion no existe
+  		ParSeq(); //Error la posicion no existe
   	}else if (movimiento < 0){
-  		List(); //Error la posicion no existe
+  		ParSeq(); //Error la posicion no existe
   	}else if(n>=7*9){
-  		List();
+  		ParSeq();
   	}
   	else if(n==posicion){
-  			List(tablero(movimiento)) ::: mover(n+1,posicion,movimiento,tablero);
+  			ParSeq(tablero(movimiento)) ++ mover(n+1,posicion,movimiento,tablero);
   		
   	}else if(n==movimiento){
-  			List( tablero(posicion)) ::: mover(n+1,posicion,movimiento,tablero);
+  			ParSeq(tablero(posicion)) ++ mover(n+1,posicion,movimiento,tablero);
   	}else{
-  			List( tablero(n)) ::: mover(n+1,posicion,movimiento,tablero); //Seguimos buscando la posicion a mover
+  			ParSeq(tablero(n)) ++ mover(n+1,posicion,movimiento,tablero); //Seguimos buscando la posicion a mover
   			
   				}
   } 
 	
-	def cambio(posicion :Int , movimiento : Int , tablero: List[Int]): List[Int] ={
+	def cambio(posicion :Int , movimiento : Int , tablero: ParSeq[Int]): ParSeq[Int] ={
 
   	if(movimiento==2){
   		if(posicion%9 == 0){
-  			List();
+  			ParSeq();
   		}else{
   			mover(0,posicion,posicion-1,tablero);
   		}
   	}else if(movimiento==3){
   		if(posicion/9 == 6){
-  			List();
+  			ParSeq();
   		}else{
   			mover(0,posicion,posicion+9,tablero);
   			}
   	}else if(movimiento==4){
   		if((posicion+1)%9 == 0){
-  			List();
+  			ParSeq();
   		}else{
   		mover(0,posicion,posicion+1,tablero);
   			}
   	}else if(movimiento==1){
   		if(posicion/9 == 0){
-  			List();
+  			ParSeq();
   		}else{
   			mover(0,posicion,posicion-9,tablero);
   		}
   	}else{
-  	List();
+  	ParSeq();
   	}
 
   }
 	
-	def fila0s(n:Int , fila:List[Int]): List[Int] ={
+	def fila0s(n:Int , fila:ParSeq[Int]): ParSeq[Int] ={
 	if(n==8){
-		List();
+		ParSeq();
 	}else if( fila(n) == fila(n+1)){
-		List(n) ::: fila0s(n+1,fila);
+		ParSeq(n) ++ fila0s(n+1,fila);
 	}else{
-		List(n) ::: fila0s(n+1,fila);
+		ParSeq(n) ++ fila0s(n+1,fila);
 		}
   }
 
  
   
-  def filanueva(n:Int , fila:List[Int]) : List[Int] = {
+  def filanueva(n:Int , fila:ParSeq[Int]) : ParSeq[Int] = {
 	
     
     
   	if(n==8){
-  		List(fila.head);
+  		ParSeq(fila.head);
   	}else{
   		val seg = seguidos(fila.length,0,fila);
  
@@ -237,12 +236,12 @@ def todas(x: Int, y : Int): Int = {
   			
   			if(n+seg>8){
   			  
-			    List.range(0,seg).map{case _ => 0} 
+			    ParSeq.range(0,seg).map{case _ => 0} 
 			}else{
-			    List.range(0,seg).map{case _ => 0} ::: filanueva(n+seg,filaux);  
+			    ParSeq.range(0,seg).map{case _ => 0} ++ filanueva(n+seg,filaux);  
 			}
   		}else{
-  			List(fila.head) ::: filanueva(n+1,fila.tail);
+  			ParSeq(fila.head) ++ filanueva(n+1,fila.tail);
   		}
   	}
   	
@@ -331,7 +330,7 @@ def todas(x: Int, y : Int): Int = {
        
      }
   
-   def pedirUser() : List[Int]= {
+   def pedirUser() : ParSeq[Int]= {
      val opt = seguir(0)
      if(opt == 2){
        println("Seleccione casilla: ")
@@ -339,78 +338,77 @@ def todas(x: Int, y : Int): Int = {
        println("\n\tPosicion y: ")
        val y = pedirY(0) -1  
        val i = pedirMov(0);     
-       List(x,y,i,opt)
+       ParSeq(x,y,i,opt)
      }else{
-       List(0,0,0,1)
+       ParSeq(0,0,0,1)
      }
    }
    
 
   
-  def colnueva(n:Int , fila:List[Int]) : List[Int] = {
+  def colnueva(n:Int , fila:ParSeq[Int]) : ParSeq[Int] = {
 
 	if(n==6){
-		List(fila.head);
+		ParSeq(fila.head);
 	}else{
 		val seg = seguidos(fila.length,0,fila);
 		if (seg >2) {
 			val filaux = fila.drop(seg)
 			if(n+seg>6){
-			     List.range(0,seg).map{case _ => 0}
+			     ParSeq.range(0,seg).map{case _ => 0}
 			}else{
-			    List.range(0,seg).map{case _ => 0} ::: colnueva(n+seg,filaux);  
+			    ParSeq.range(0,seg).map{case _ => 0} ++ colnueva(n+seg,filaux);  
 			}
 			
 		}else{
-			List(fila.head) ::: colnueva(n+1,fila.tail);
+			ParSeq(fila.head) ++ colnueva(n+1,fila.tail);
 		}
 	}
 	
 	
 } 
   
-  def actualizaTablero(lst: List[Int]): List[Int] = {
-  	val c1 = leerColumna(lst,0,0,9,List())
-  	val c2 = leerColumna(lst,0,1,9,List())
-  	val c3 = leerColumna(lst,0,2,9,List())
-  	val c4 = leerColumna(lst,0,3,9,List())
-  	val c5 = leerColumna(lst,0,4,9,List())
-  	val c6 = leerColumna(lst,0,5,9,List())
-  	val c7 = leerColumna(lst,0,6,9,List())
-  	val c8 = leerColumna(lst,0,7,9,List())
-  	val c9 = leerColumna(lst,0,8,9,List())
+  def actualizaTablero(lst: ParSeq[Int]): ParSeq[Int] = {
+  	val c1 = leerColumna(lst,0,0,9,ParSeq())
+  	val c2 = leerColumna(lst,0,1,9,ParSeq())
+  	val c3 = leerColumna(lst,0,2,9,ParSeq())
+  	val c4 = leerColumna(lst,0,3,9,ParSeq())
+  	val c5 = leerColumna(lst,0,4,9,ParSeq())
+  	val c6 = leerColumna(lst,0,5,9,ParSeq())
+  	val c7 = leerColumna(lst,0,6,9,ParSeq())
+  	val c8 = leerColumna(lst,0,7,9,ParSeq())
+  	val c9 = leerColumna(lst,0,8,9,ParSeq())
   
   	
   	val aux1 = c1.filter(_ != 0)
-  	val f1 =  List.range(0,7 - aux1.length).map{case _ => 1 + scala.util.Random.nextInt(6)} ++ aux1
+  	val f1 =  ParSeq.range(0,7 - aux1.length).map{case _ => 1 + scala.util.Random.nextInt(6)} ++ aux1
   	
-  	val aux2 = c2.filter(_ != 0)
-  	val f2 =  List.range(0,7 - aux2.length).map{case _ => 1 + scala.util.Random.nextInt(6)} ++ aux2
+  	val aux2 = c2.par.filter(_ != 0)
+  	val f2 =  ParSeq.range(0,7 - aux2.length).par.map{case _ => 1 + scala.util.Random.nextInt(6)} ++ aux2
   
-  	val aux3 = c3.filter(_ != 0)
-  	val f3 =  List.range(0,7 - aux3.length).map{case _ => 1 + scala.util.Random.nextInt(6)} ++ aux3 
+  	val aux3 = c3.par.filter(_ != 0)
+  	val f3 =  ParSeq.range(0,7 - aux3.length).par.map{case _ => 1 + scala.util.Random.nextInt(6)} ++ aux3
   
-  	val aux4 = c4.filter(_ != 0)
-  	val f4 =  List.range(0,7 - aux4.length).map{case _ => 1 + scala.util.Random.nextInt(6)} ++ aux4
+  	val aux4 = c4.par.filter(_ != 0)
+  	val f4 =  ParSeq.range(0,7 - aux4.length).par.map{case _ => 1 + scala.util.Random.nextInt(6)} ++ aux4
   
-  	val aux5 = c5.filter(_ != 0)
-  	val f5 =  List.range(0,7 - aux5.length).map{case _ => 1 + scala.util.Random.nextInt(6)} ++ aux5
+  	val aux5 = c5.par.filter(_ != 0)
+  	val f5 =  ParSeq.range(0,7 - aux5.length).par.map{case _ => 1 + scala.util.Random.nextInt(6)} ++ aux5
   
-  	val aux6 = c6.filter(_ != 0)
-  	val f6 =  List.range(0,7 - aux6.length).map{case _ => 1 + scala.util.Random.nextInt(6)} ++ aux6
+  	val aux6 = c6.par.filter(_ != 0)
+  	val f6 =  ParSeq.range(0,7 - aux6.length).par.map{case _ => 1 + scala.util.Random.nextInt(6)} ++ aux6
   
-  	val aux7 = c7.filter(_ != 0)
-  	val f7 =  List.range(0,7 - aux7.length).map{case _ => 1 + scala.util.Random.nextInt(6)} ++ aux7
+  	val aux7 = c7.par.filter(_ != 0)
+  	val f7 = ParSeq.range(0,7 - aux7.length).par.map{case _ => 1 + scala.util.Random.nextInt(6)} ++ aux7
   
-  	val aux8 = c8.filter(_ != 0)
-  	val f8 =  List.range(0,7 - aux8.length).map{case _ => 1 + scala.util.Random.nextInt(6)} ++ aux8
-  
+  	val aux8 = c8.par.filter(_ != 0)
+  	val f8 = ParSeq.range(0,7 - aux8.length).par.map{case _ => 1 + scala.util.Random.nextInt(6)} ++ aux8
  
-    val aux9 = c9.filter(_ != 0)
-  	val f9 =  List.range(0,7 - aux9.length).map{case _ => 1 + scala.util.Random.nextInt(6)} ++ aux9
+    val aux9 = c9.par.filter(_ != 0)
+  	val f9 =  ParSeq.range(0,7 - aux9.length).par.map{case _ => 1 + scala.util.Random.nextInt(6)} ++ aux9
 
   
-  	val lf = matrizColumnas(f1,f2,f3,f4,f5,f6,f7,f8,f9,List())
+  	val lf = matrizColumnas(f1,f2,f3,f4,f5,f6,f7,f8,f9,ParSeq())
   	lf
   
   
@@ -418,27 +416,26 @@ def todas(x: Int, y : Int): Int = {
   
   }
   
-  def eliminarTres(l: List[Int]): List[Int] = {
-		val f1 = leerFila(l, 0, 0, 9, List())
-		val f2 = leerFila(l, 0, 1, 9, List())
-		val f3 = leerFila(l, 0, 2, 9, List())
-		val f4 = leerFila(l, 0, 3, 9, List())
-		val f5 = leerFila(l, 0, 4, 9, List())
-		val f6 = leerFila(l, 0, 5, 9, List())
-		val f7 = leerFila(l, 0, 6, 9, List())
+  def eliminarTres(l: ParSeq[Int]): ParSeq[Int] = {
+		val f1 = leerFila(l, 0, 0, 9, ParSeq())
+		val f2 = leerFila(l, 0, 1, 9, ParSeq())
+		val f3 = leerFila(l, 0, 2, 9, ParSeq())
+		val f4 = leerFila(l, 0, 3, 9, ParSeq())
+		val f5 = leerFila(l, 0, 4, 9, ParSeq())
+		val f6 = leerFila(l, 0, 5, 9, ParSeq())
+		val f7 = leerFila(l, 0, 6, 9, ParSeq())
 
 
-		val c1 = leerColumna(l, 0, 0, 9, List())
-		val c2 = leerColumna(l, 0, 1, 9, List())
-		val c3 = leerColumna(l, 0, 2, 9, List())
-		val c4 = leerColumna(l, 0, 3, 9, List())
-		val c5 = leerColumna(l, 0, 4, 9, List())
-		val c6 = leerColumna(l, 0, 5, 9, List())
-		val c7 = leerColumna(l, 0, 6, 9, List())
-		val c8 = leerColumna(l, 0, 7, 9, List())
-		val c9 = leerColumna(l, 0, 8, 9, List())
-		
-	
+		val c1 = leerColumna(l, 0, 0, 9, ParSeq())
+		val c2 = leerColumna(l, 0, 1, 9, ParSeq())
+		val c3 = leerColumna(l, 0, 2, 9, ParSeq())
+		val c4 = leerColumna(l, 0, 3, 9, ParSeq())
+		val c5 = leerColumna(l, 0, 4, 9, ParSeq())
+		val c6 = leerColumna(l, 0, 5, 9, ParSeq())
+		val c7 = leerColumna(l, 0, 6, 9, ParSeq())
+		val c8 = leerColumna(l, 0, 7, 9, ParSeq())
+		val c9 = leerColumna(l, 0, 8, 9, ParSeq())
+			
 		
 		val a1 = filanueva(0, f1)
 		val a2 = filanueva(0, f2)
@@ -459,14 +456,14 @@ def todas(x: Int, y : Int): Int = {
 		val b9 = colnueva(0, c9)
 		
 		val a = matrizFilas(a1, a2, a3, a4, a5, a6, a7)
-		val b = matrizColumnas(b1, b2, b3, b4, b5, b6, b7, b8, b9, List())
+		val b = matrizColumnas(b1, b2, b3, b4, b5, b6, b7, b8, b9, ParSeq())
 
 		
-	  comparaMatrices(a, b, List())
+	  comparaMatrices(a, b, ParSeq())
 		
 	} 
   
-  def turno(control : Int, tablero:List[Int]): List[Int] ={
+  def turno(control : Int, tablero:ParSeq[Int]): ParSeq[Int] ={
     
     println("Tablero")
     imprimir(0,tablero)
@@ -499,7 +496,7 @@ def todas(x: Int, y : Int): Int = {
     }
   }
   
-  def bucleTablero(tablero:List[Int]): List[Int] ={
+  def bucleTablero(tablero:ParSeq[Int]): ParSeq[Int] ={
     //eliminar 3 
     
     val nuevo = eliminarTres(tablero);
@@ -517,12 +514,12 @@ def todas(x: Int, y : Int): Int = {
     
    
 def main(args:Array[String]) { 
-    
+     
     //Declaramos objetos
     
     
     //Generamos el tablero
-    val tablero = generarTablero(7*9, List());   
+    val tablero = generarTablero(7*9, ParSeq());   
    
     //bucle
     
@@ -531,7 +528,8 @@ def main(args:Array[String]) {
     println("Fin ejecucion")
     
     
-    
+
+
    
   }
 }
